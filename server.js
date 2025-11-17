cb96dd8af2ab4411113197a91780dc0fe4044396
oKHassan <freelancerhassan3@gmail.com>
2025-11-17 12:13:10 +0600

    fix err-1

--- server.js
+++ server.js
@@ -28,31 +28,15 @@ connectDB();
 import cors from "cors";
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        console.log("Incoming origin:", origin);
-
-        const allowedOrigins = [
-            'http://192.168.98.73:5173',
-            'http://localhost:5173',
-            'https://maison-frontend-eta.vercel.app',
-        ];
-
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    origin: "*", // allow ALL domains
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false, // must be false when origin="*"
 };
 
-// ⭐ must handle preflight
-app.options('*', cors(corsOptions));
-
-// ⭐ apply cors
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
